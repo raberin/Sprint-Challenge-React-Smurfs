12230680cefcc52944a5fa101730d23a5c9077ba
raberin <rmvaberin@gmail.com>
2019-04-07 19:13:19 -0700

    Created navigation bar. Added CSS to nav bar.

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Route } from "react-router-dom";
 
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
   constructor(props) {
@@ -43,8 +45,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Navigation />
+        <Route
+          path="/"
+          exact
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+        <Route
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} addSmurf={this.addSmurf} />}
+        />
       </div>
     );
   }
